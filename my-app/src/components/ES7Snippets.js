5104572837beeaeda32e9234ed75bd1eec37267f
Molly Adamsson <adamssonmolly@gmail.com>
2023-03-24 11:06:58 +0000

    Added functions

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import React, { Component } from 'react'
+
+function ES7Snippets() {
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default ES7Snippets
+
+const ES7Snippets = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ES7Snippets
+
+
+export class ES7Snippets extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ES7Snippets
